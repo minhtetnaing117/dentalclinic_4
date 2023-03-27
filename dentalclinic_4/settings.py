48421897f4649b987284f617d644f722247a2661
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2023-03-27 23:52:38 +0630

    .

--- dentalclinic_4/settings.py
+++ dentalclinic_4/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hta_e)2@0p9_^nmsi-w@a1v6axnlh0*a(m82nqn(brmuczqk_k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.*']
 
 
 # Application definition
