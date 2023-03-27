759aa6e5ee1f5c39852426d326183f1cc52db38d
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2023-03-28 00:16:09 +0630

    new

--- dentalclinic_4/settings.py
+++ dentalclinic_4/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
-from decouple import config
+# import dj_database_url
+# from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,20 +77,28 @@ WSGI_APPLICATION = 'dentalclinic_4.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    # #    'ENGINE': 'django.db.backends.sqlite3',
+    # #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     # 'ENGINE'   : 'django.db.backends.mysql',
+    #     'ENGINE'   : 'django.db.backends.postgresql',
+    #     'NAME'     : 'dentalclinic', # name of the database
+    #     'USER'     : 'admin', # username of the database 
+    #     'PASSWORD' : 'admin', #password of your postgresql app
+    #     'HOST'     : 'localhost', # name of the host
+    #     'PORT'     : '5432',
+    # }
     'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE'   : 'django.db.backends.mysql',
         'ENGINE'   : 'django.db.backends.postgresql',
-        'NAME'     : 'dentalclinic', # name of the database
-        'USER'     : 'admin', # username of the database 
-        'PASSWORD' : 'admin', #password of your postgresql app
-        'HOST'     : 'localhost', # name of the host
-        'PORT'     : '5432',
+        'NAME'     : 'railway', # name of the database
+        'USER'     : 'postgres', # username of the database 
+        'PASSWORD' : 'YewITxHTm3R1ISa3HVMN', #password of your postgresql app
+        'HOST'     : 'containers-us-west-46.railway.app', # name of the host
+        'PORT'     : '7561',
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -128,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+# STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
